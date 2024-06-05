4211a921bba9fb514a3075a95bd18c3237fd7bbf
tamioka-giyu <122829351+tamioka-giyu@users.noreply.github.com>
2024-06-05 18:31:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import tkinter as tk
 import tkinter.font as tkFont
 
-
 def button_command():
-    print("command")
-
+    label["text"] = "Михайло"
 
 if __name__ == "__main__":
     root = tk.Tk()
